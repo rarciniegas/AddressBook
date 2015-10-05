2d2dc31656418c961fc832e2c4787924dd654bd3
rarciniegas <rarci003@fiu.edu>
2015-10-05 16:20:48 -0400

    End of the day at the Lab

---
+++ Address Book/TestProgram.m
@@ -0,0 +1,47 @@
+//
+//  TestProgram.m
+//  Address Book
+//
+//  Created by roberto arciniegas on 10/5/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+/*
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+        NSString *aName = @"Julia Kochan";
+        NSString *aEmail = @"jewls337@axlc.com";
+        NSString *bName = @"Tony Iannino";
+        NSString *bEmail = @"tony.iannino@techfitness.com"; NSString *cName = @"Stephen Kochan";
+        NSString *cEmail = @"steve@classroomM.com"; NSString *dName = @"Jamie Baker";
+        NSString *dEmail = @"jbaker@classroomM.com";
+        AddressCard * card1 = [[AddressCard alloc] init];
+        AddressCard * card2 = [[AddressCard alloc] init];
+        AddressCard * card3 = [[AddressCard alloc] init];
+        AddressCard * card4 = [[AddressCard alloc] init];
+        
+        AddressBook * myBook = [AddressBook alloc];
+        
+        // First set up four address cards
+        [card1 setName: aName andEmail: aEmail];
+        [card2 setName: bName andEmail: bEmail];
+        [card3 setName: cName andEmail: cEmail];
+        [card4 setName: dName andEmail: dEmail];
+        
+        myBook = [myBook initWithName: @"Linda’s Address book"];
+        
+        // Add some cards to the address book
+        [myBook addCard: card1];
+        [myBook addCard: card2];
+        [myBook addCard: card3];
+        [myBook addCard: card4];
+        // List the unsorted book [myBook list];
+        // Sort it and list it again
+        [myBook sort];
+        [myBook list];
+    }
+    return 0;
+}
+ */
